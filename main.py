298bed181fcb08252326220ba401c3945c6a0059
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2021-12-16 20:48:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 from ulti import *
 import threading
+from PIL import Image
+import requests
+from io import BytesIO
+
+
 # from streamlit_webrtc import (
 #     AudioProcessorBase,
 #     ClientSettings,
@@ -70,7 +75,27 @@ def main():
                 st.image(prediction)
     
 
-    elif choice == 'Video Based':
+    elif choice == 'URL':
+        response = requests.get(url)
+        Image = Image.open(BytesIO(response.content))
+        
+        if Image is not None:
+            col1, col2 = st.beta_columns(2)
+            Image = Image.read()
+            Image = tf.image.decode_image(Image, channels=3).numpy()                  
+            Image = adjust_gamma(Image, gamma=gamma)
+            with col1:
+                st.image(Image)
+            input_image = loadtest(Image,cropornot=Autocrop)
+            prediction = comic_model(input_image, training=True)
+            prediction = tf.squeeze(prediction,0)
+            prediction = prediction* 0.5 + 0.5
+            prediction = tf.image.resize(prediction, 
+                           [outputsize, outputsize],
+                           method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+            prediction=  prediction.numpy()
+            with col2:
+                st.image(prediction)
 
          #class OpenCVVideoProcessor(VideoProcessorBase):
         #     def __init__(self) -> None:
