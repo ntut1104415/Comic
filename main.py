731fdc91b2060e758bb7c7f58e2547feaa75be7b
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2021-12-23 20:25:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -51,9 +51,10 @@ def main():
         
         st.sidebar.header('配置')
         outputsize = st.sidebar.selectbox('輸出尺寸', [384,512,768])
+        mode = st.sidebar.selectbox('模式選擇', ['漫畫風格','油畫風格'])
         Autocrop = st.sidebar.checkbox('自動裁剪照片',value=True) 
         gamma = st.sidebar.slider('Gamma 調整', min_value=0.1, max_value=3.0,value=1.0,step=0.1) # change the value here to get different result
-        mode = st.sidebar.selectbox('模式選擇', ['漫畫風格','油畫風格'])
+
 
         Image = st.file_uploader('在這上傳您的檔案',type=['jpg','jpeg','png'])
         if Image is not None:
@@ -79,9 +80,10 @@ def main():
     elif choice == 'URL':
         st.sidebar.header('配置')
         outputsize = st.sidebar.selectbox('輸出尺寸', [384,512,768])
+        mode = st.sidebar.selectbox('模式選擇', ['漫畫風格','油畫風格'])
         Autocrop = st.sidebar.checkbox('自動裁剪照片',value=True) 
         gamma = st.sidebar.slider('Gamma 調整', min_value=0.1, max_value=3.0,value=1.0,step=0.1) # change the value here to get different result
-        mode = st.sidebar.selectbox('模式選擇', ['漫畫風格','油畫風格'])
+        
         url = st.text_input('網址連結')
         response = requests.get(url)
        # st.write(response.content)
