b92778152641a6cf80255ddf37e4995facc69b76
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2021-12-23 20:13:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -81,8 +81,8 @@ def main():
         outputsize = st.sidebar.selectbox('輸出尺寸', [384,512,768])
         Autocrop = st.sidebar.checkbox('自動裁剪照片',value=True) 
         gamma = st.sidebar.slider('Gamma 調整', min_value=0.1, max_value=3.0,value=1.0,step=0.1) # change the value here to get different result
-        
-        url = st.text_input('The URL link')
+        mode = st.sidebar.selectbox('模式選擇', ['漫畫風格','油畫風格'])
+        url = st.text_input('網址連結')
         response = requests.get(url)
        # st.write(response.content)
         Image = (response.content)
