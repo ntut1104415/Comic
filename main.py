b44c7864c6e22313958190098aa8bcbbad2b827c
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2021-12-23 21:14:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import streamlit as st
 #         "audio": False,
 #     },)
 
-model_path = os.path.join('model','ModelTrainOnKaggle.h5','candy.t7')
+model_path = os.path.join('model','ModelTrainOnKaggle.h5')
 @st.cache
 def model_load():
     model = tf.keras.models.load_model(model_path)
