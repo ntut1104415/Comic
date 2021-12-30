2c186570de8611a8c8825a3f7b472bd183e3fe17
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2021-12-30 20:10:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -57,23 +57,23 @@ def main():
 
         #if mode == '漫畫風格':
             #Image = st.file_uploader('在這上傳您的檔案',type=['jpg','jpeg','png'])
-            if Image is not None:
-                col1, col2 = st.beta_columns(2)
+            #if Image is not None:
+                #col1, col2 = st.beta_columns(2)
                 Image = Image.read()
-                Image = tf.image.decode_image(Image, channels=3).numpy()                  
-                Image = adjust_gamma(Image, gamma=gamma)
-                with col1:
-                    st.image(Image)
-                input_image = loadtest(Image,cropornot=Autocrop)
-                prediction = comic_model(input_image, training=True)
-                prediction = tf.squeeze(prediction,0)
-                prediction = prediction* 0.5 + 0.5
-                prediction = tf.image.resize(prediction, 
-                            [outputsize, outputsize],
-                            method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-                prediction=  prediction.numpy()
-                with col2:
-                    st.image(prediction)
+                #Image = tf.image.decode_image(Image, channels=3).numpy()                  
+                #Image = adjust_gamma(Image, gamma=gamma)
+                #with col1:
+                    #st.image(Image)
+                #input_image = loadtest(Image,cropornot=Autocrop)
+                #prediction = comic_model(input_image, training=True)
+                #prediction = tf.squeeze(prediction,0)
+                #prediction = prediction* 0.5 + 0.5
+                #prediction = tf.image.resize(prediction, 
+                            #[outputsize, outputsize],
+                            #method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+                #prediction=  prediction.numpy()
+                #with col2:
+                    #st.image(prediction)
             #elif mode == '油畫風格':
 
     elif choice == 'URL':
