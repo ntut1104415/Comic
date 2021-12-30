10971fa24359c10f8b737bf39965e5bd365b8ca4
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2021-12-30 20:12:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def main():
                 prediction=  prediction.numpy()
                 with col2:
                     st.image(prediction)
-            elif mode == '油畫風格':
+            #elif mode == '油畫風格':
 
     elif choice == 'URL':
         st.sidebar.header('配置')
