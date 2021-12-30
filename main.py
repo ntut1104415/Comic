69f96b811499b005802b24366d75ac53e8797991
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2021-12-30 20:07:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def main():
         Autocrop = st.sidebar.checkbox('自動裁剪照片',value=True) 
         #gamma = st.sidebar.slider('Gamma 調整', min_value=0.1, max_value=3.0,value=1.0,step=0.1) # change the value here to get different result
 
-        #if mode == '漫畫風格':
+        if mode == '漫畫風格':
             Image = st.file_uploader('在這上傳您的檔案',type=['jpg','jpeg','png'])
             if Image is not None:
                 col1, col2 = st.beta_columns(2)
