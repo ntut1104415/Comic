86cd18b82b4af2a98a346e50d842c023639b9995
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2021-12-30 20:02:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,10 +50,10 @@ def main():
     if choice == 'Image Based':
         
         st.sidebar.header('配置')
-        mode = st.sidebar.selectbox('模式選擇', ['漫畫風格','油畫風格'])
+        #mode = st.sidebar.selectbox('模式選擇', ['漫畫風格','油畫風格'])
         outputsize = st.sidebar.selectbox('輸出尺寸', [384,512,768])
         Autocrop = st.sidebar.checkbox('自動裁剪照片',value=True) 
-        gamma = st.sidebar.slider('Gamma 調整', min_value=0.1, max_value=3.0,value=1.0,step=0.1) # change the value here to get different result
+        #gamma = st.sidebar.slider('Gamma 調整', min_value=0.1, max_value=3.0,value=1.0,step=0.1) # change the value here to get different result
 
         if mode == '漫畫風格':
             Image = st.file_uploader('在這上傳您的檔案',type=['jpg','jpeg','png'])
